beb818c29cc21b463f2323cf707d2e604f184735
Baylee Feore <baylee@yetihq.com>
2014-10-27 09:37:32 -0700

    Initial addition of pushwoosh

--- rest_notifications/utils.py
+++ rest_notifications/utils.py
@@ -1,10 +1,19 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.utils.html import strip_tags
+from pypushwoosh.client import PushwooshClient
+from pypushwoosh.command import CreateTargetedMessageCommand
+from pypushwoosh.filter import ApplicationFilter
 
 
 def send_push_notification(receiver, message):
-    pass
+    command = CreateTargetedMessageCommand()
+    command.auth = 'AUTH_TOKEN'
+    command.devices_filter = ApplicationFilter('APP-CODE')
+    command.content = "Hello world!"
+
+    client = PushwooshClient()
+    print client.invoke(command)
 
 
 def send_email_notification(receiver, message, reply_to=None):
